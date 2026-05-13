0ab202902261732aa7a2bb85c16d3f45929dbcbb
code08 <bogdanovdb6694795@yandex.ru>
2026-05-13 10:01:21 +0300

    Сделан метод MustInitQdrantCollection

--- src/search_engine/internal/setup/qdrant_init.go
+++ src/search_engine/internal/setup/qdrant_init.go
@@ -2,28 +2,38 @@ package setup
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/qdrant/go-client/qdrant"
 )
 
-func InitQdrantСlient(ctx context.Context, host string, port int) (*qdrant.Client, error) {
+func MustInitQdrantСlient(ctx context.Context, host string, port int) *qdrant.Client {
 	cli, err := qdrant.NewClient(&qdrant.Config{
 		Host: host,
 		Port: port,
 	})
 
 	if err != nil {
-		return nil, err
+		panic(fmt.Sprintf("qdrant init failed: %v", err))
 	}
 
 	_, err = cli.HealthCheck(ctx)
 	if err != nil {
-		return nil, err
+		panic(fmt.Sprintf("qdrant check health failed: %v", err))
 	}
 
-	return cli, nil
+	return cli
 }
 
-func InitQdrantCollections() {
-
+func MustInitQdrantCollections(client *qdrant.Client, collection_name string, vec_size uint64, distance_type qdrant.Distance) {
+	err := client.CreateCollection(context.Background(), &qdrant.CreateCollection{
+		CollectionName: collection_name,
+		VectorsConfig: qdrant.NewVectorsConfig(&qdrant.VectorParams{
+			Size:     vec_size,
+			Distance: distance_type,
+		}),
+	})
+	if err != nil {
+		panic(err)
+	}
 }
