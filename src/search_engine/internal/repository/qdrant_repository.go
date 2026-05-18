5bac7382975e03a79c153557d19ef8f3e24fd03a
Code08 <100559620+Code0871@users.noreply.github.com>
2026-05-18 17:09:52 +0300

    Searcher (#15)
    * Написан python сервис с ИИ эмбеддинговыми моделями для поискового движка
    
    * Написан python сервис с ИИ эмбеддинговыми моделями для поискового движка
    
    * Доделан слой repository поискового движка
    
    * исключена папка qdrant_storage
    
    ---------
    
    Co-authored-by: code08 <bogdanovdb6694795@yandex.ru>

--- src/search_engine/internal/repository/qdrant_repository.go
+++ src/search_engine/internal/repository/qdrant_repository.go
@@ -4,20 +4,23 @@ import (
 	"context"
 	"search_engine/config"
 
+	"fmt"
+	"log"
+	"time"
+
 	"github.com/google/uuid"
 	"github.com/qdrant/go-client/qdrant"
 )
 
-// TODO: Сделать функцию для обновления коллекции
-
 type QdrantRepository interface {
 	// CRUD операции
 	Upsert(ctx context.Context, collection_name string, vector []float32, passport, name, mi_type string) error
-	Delete(ctx context.Context, collection_name string, ids []uint64) error
+	UpsertBatch(ctx context.Context, collection_name string, points []*qdrant.PointStruct) error
+	GetPoints(ctx context.Context, collection_name string, ids []string) ([]*qdrant.RetrievedPoint, error)
+	Delete(ctx context.Context, collection_name string, passport string) error
 
 	// Поиск
 	FindNearest(ctx context.Context, collection_name string, vector []float32) ([]*qdrant.ScoredPoint, error)
-	//Search(ctx context.Context, collection_name string, vector []float32) ([]*qdrant.ScoredPoint, error)
 	SearchWithFilter(ctx context.Context, collection_name string, vector []float32, filter *qdrant.Filter) ([]*qdrant.ScoredPoint, error)
 
 	Close() error
@@ -57,6 +60,60 @@ func (qr *qdrantRepository) Upsert(ctx context.Context, collection_name string, 
 	return nil
 }
 
+// Пакетная вставка точек
+func (qr *qdrantRepository) UpsertBatch(ctx context.Context, collection_name string, points []*qdrant.PointStruct) error {
+	batch_size := 250
+	for i := 0; i < len(points); i += batch_size {
+		end := i + batch_size
+		if end > len(points) {
+			end = len(points)
+		}
+		batch := points[i:end]
+
+		upsert_request := &qdrant.UpsertPoints{
+			CollectionName: collection_name,
+			Points:         batch,
+			Wait:           qdrant.PtrOf(false),
+		}
+
+		_, err := qr.client.Upsert(ctx, upsert_request)
+		if err != nil {
+			log.Printf("Ошибка вставки батча: [%d:%d]: %v", i, end, err)
+			continue
+		}
+
+		log.Printf("Батч [%d:%d] успешно вставлен", i, end)
+		time.Sleep(30 * time.Millisecond)
+	}
+
+	return nil
+}
+
+// получаем точку(и) по ID
+func (qr *qdrantRepository) GetPoints(ctx context.Context, collection_name string, ids []string) ([]*qdrant.RetrievedPoint, error) {
+	if len(ids) == 0 {
+		return nil, nil
+	}
+
+	point_ids := make([]*qdrant.PointId, 0, len(ids))
+
+	for _, id := range ids {
+		point_ids = append(point_ids, qdrant.NewID(id))
+	}
+
+	retrieved_points, err := qr.client.Get(ctx, &qdrant.GetPoints{
+		CollectionName: collection_name,
+		Ids:            point_ids,
+		WithPayload:    qdrant.NewWithPayload(true),
+	})
+
+	if err != nil {
+		return nil, fmt.Errorf("ошибка получения точек: %w", err)
+	}
+
+	return retrieved_points, nil
+}
+
 // функция поиска ближайших векторов
 func (qr *qdrantRepository) FindNearest(ctx context.Context, collection_name string, vector []float32) ([]*qdrant.ScoredPoint, error) {
 	search_result, err := qr.client.Query(ctx, &qdrant.QueryPoints{
@@ -68,13 +125,30 @@ func (qr *qdrantRepository) FindNearest(ctx context.Context, collection_name str
 	})
 
 	if err != nil {
+		log.Printf("Ошибка поиска: %v", err)
 		return nil, err
 	}
 
 	return search_result, nil
 }
 
-func (qr *qdrantRepository) Delete(ctx context.Context, collection_name string, ids []uint64) error {
+// Функция удаления точки по совпадению паспорта СИ в payload'е
+func (qr *qdrantRepository) Delete(ctx context.Context, collection_name string, passport string) error {
+	_, err := qr.client.Delete(ctx, &qdrant.DeletePoints{
+		CollectionName: collection_name,
+		Points: qdrant.NewPointsSelectorFilter(
+			&qdrant.Filter{
+				Must: []*qdrant.Condition{
+					qdrant.NewMatch("passport", passport),
+				},
+			},
+		),
+	})
+
+	if err != nil {
+		log.Printf("Ошибка удаления: %v", err)
+		return err
+	}
 	return nil
 }
 
