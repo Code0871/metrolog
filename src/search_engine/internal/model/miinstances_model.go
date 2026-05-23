b4cf1de3e958b862eccd7a86f84df825efd42064
code0871 <bogdanovdb6694795@yandex.ru>
2026-05-23 13:53:13 +0300

    Почищен слой DTO в поисковом сервисе

--- src/search_engine/internal/model/miinstances_model.go
+++ src/search_engine/internal/model/miinstances_model.go
@@ -16,18 +16,6 @@ type UpsertRequest struct {
 	MiType   string `json:"mi_type" validate:"required"`
 }
 
-// Структура результата поиска
-type SearchResult struct {
-	Id      string                 `json:"id"`
-	Score   float32                `json:"score"`
-	Payload map[string]interface{} `json:"payload"`
-}
-
-// Струкутра ответа на запрос поиска
-type SearchResponses struct {
-	Responses []SearchResult `json:"responses"`
-}
-
 // Структура запроса на пакетный поиск
 type BatchSearchRequest struct {
 	Texts []SearchRequest `json:"texts" validate:"required"`
@@ -36,37 +24,6 @@ type BatchSearchRequest struct {
 // Структура запроса на пакетную вставку
 type BatchUpsertRequest struct {
 	UpsertRequests []UpsertRequest `json:"upsert_requests" validate:"required"`
-}
-
-type PointRequest struct {
-	ID string `json:"id"`
-}
-
-type PointResponse struct {
-	ID      string                 `json:"id"`
-	Payload map[string]interface{} `json:"payload"`
-	Vector  []float32              `json:"vector,omitempty"`
-}
-
-type BatchPointRequest struct {
-	Points []PointRequest `json:"points"`
-}
-
-type BatchPointResponse struct {
-	Points []PointResponse `json:"points"`
-}
-
-type HybridSearchRequest struct {
-	DenseVector  []float32    `json:"dense_vector"`
-	SparseVector SparseVector `json:"sparse_vector"`
-	MultiVector  [][]float32  `json:"multi_vector"`
-}
-
-// PointData - структура для вставки точки
-type PointData struct {
-	ID      string                 `json:"id"`
-	Payload map[string]interface{} `json:"payload"`
-	Vectors map[string]interface{} `json:"vectors"` // dense, sparse, multi вектора
 }
 
 type Miinstance struct {
